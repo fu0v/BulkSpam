c32743ff0709791434d33634a6cd1ed3793f6908
wahyuandhika <43238100+wahyuandhika@users.noreply.github.com>
2018-12-20 15:33:56 +0700

    Update bulk.py

--- bulk.py
+++ bulk.py
@@ -4,9 +4,7 @@ os.system('figlet "BulkSpam" |lolcat')
 print 55*"\033[33;5m="
 em = raw_input("\033[35;5mEmail Target: \033[32;5m")
 wikwik =  ["81243526382", "812346257388", "827362625362", "81243264625", "821364628482", "81249526782", "812346157388", "824362625362", "81243224625", "821364828482", "81243616178"]
-hea = ["Mozilla/5.0 (Android 7.1.2; Mobile; rv:64.0) Gecko/64.0 Firefox/64.0", "Mozilla/5.0 (Linux; Android 7.1.2; Redmi 4X Build/N2G47H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Mobile Safari/537.36", "Mozilla/5.0 (Linux; U; Android 7.1.2; id-id; Redmi 4X Build/N2G47H) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/61.0.3163.128 Mobile Safari/537.36 XiaoMi/MiuiBrowser/9.7.3", "Opera/9.80 (Android; Opera Mini/36.2.2254/127.81; U; id) Presto/2.12.423 Version/12.16"]
-heade = random.choice(hea)
-header = {'user-agent': (heade)}
+
 def kedua(email):
   global f
   f = 2
@@ -52,7 +50,7 @@ def Tod():
           'vsmsid': '3'
         }
         url = ('https://www2.bulksms.com/register/')
-        data = requests.request("post", url, headers=header, data=key)
+        data = requests.request("post", url, data=key)
 
         c +=1
         if 'You have been successfully registered' in data.text:
